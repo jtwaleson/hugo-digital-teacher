465ee137e46d03d19a9a7bb54116e25cdd688faf
Jouke Waleson <jouke@waleson.com>
2021-07-06 20:20:39 +0200

    fix same number repeating in quiz mode

--- src/components/QuizTable.vue
+++ src/components/QuizTable.vue
@@ -30,12 +30,17 @@ export default {
         }
         await sayText(this.language === 'nl' ? `We gaan 10 keer iets oefenen uit de tafel van ${this.table}` : `We're going to practice 10 numbers from the table of ${this.table}`, this.language);
         await sleep(800);
+        let lastNumber = null;
         for (let i = 1; i <= 10; i++) {
-            let x = Math.floor(Math.random() * 10 + 1);
+            let x = null;
+            while (x === lastNumber) {
+                x = Math.floor(Math.random() * 10 + 1);
+            }
             await hearNumber(this.language === 'nl' ? `Wat is ${x} keer ${this.table}` : `What is ${x} times ${this.table}`, x * this.table, this.language);
             await sleep(100);
             await giveCompliment(this.language);
             await sleep(200);
+            lastNumber = x;
         }
         await sleep(800);
         this.running = false;
