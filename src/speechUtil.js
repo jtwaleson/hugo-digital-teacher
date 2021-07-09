a60045c680fdfdba177608a6f6e02e467eb9b7ce
Jouke Waleson <jouke@waleson.com>
2021-07-09 16:02:40 +0200

    yo

--- src/speechUtil.js
+++ src/speechUtil.js
@@ -26,7 +26,7 @@ export function sayText(text, language) {
     }
     return new Promise((resolve, reject) => {
         let utterance = new SpeechSynthesisUtterance(text);
-//        utterance.lang = window.voice.lang;//.replace("_", "-");//langToLocaleMap[language];
+        utterance.lang = window.voice.lang.replace("_", "-");//langToLocaleMap[language]; THIS IS NEEDED FOR ANDROID
         utterance.voice = window.voice;
         utterance.onend = () => {
             resolve();
